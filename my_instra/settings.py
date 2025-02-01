74178939b71b751f91ec204e638d2050fe2bed17
Santhosh1507 <webdesign1507@gmail.com>
2025-02-01 14:25:01 +0530

    first commit

--- my_instra/settings.py
+++ my_instra/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bi%n%+tg3s)fyn5$#1=7f559*n=4v8b=togkeo6c9rgaj%(=x@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1","django-instragram.onrender.com"]
 
